1f5f5608a700aaf01815063f0c0955a009d48a1e
William Kent <wjk011@gmail.com>
2023-08-03 17:14:48 -0400

    Add functions to s-contact.php

--- php/s-contact.php
+++ php/s-contact.php
@@ -15,6 +15,84 @@ if (isset($_SESSION["role"])) {
     header("Location: login.php");
     exit;
 }
+
+function get_real_name($conn, $user_name) {
+    $stmt = $conn->prepare(
+        "SELECT STUDENT_USER.REAL_NAME " .
+        "FROM STUDENT_USER " .
+        "WHERE STUDENT_USER.USER_NUMBER = ?;"
+    );
+    $stmt->bind_param("s", $user_name);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $row = $result->fetch_assoc();
+    if (!isset($row["REAL_NAME"])) {
+        die("REAL_NAME for user " . $user_name . " not returned from database");
+    }
+    return $row["REAL_NAME"];
+}
+
+function get_all_classes($conn, $user_name) {
+    $stmt = $conn->prepare(
+        "SELECT C.CLASS_NUMBER AS CLASS_NUMBER " .
+        "FROM STUDENT_USER S " .
+        "INNER JOIN STUDENT_IN_CLASS SC ON SC.STUDENT_NUMBER = S.USER_NUMBER " .
+        "INNER JOIN EDU_CLASS C ON C.CLASS_NUMBER = SC.CLASS_NUMBER " .
+        "WHERE S.USER_NAME = ?;"
+    );
+    $stmt->bind_param("s", $user_name);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $retval = array();
+    while ($row = $result->fetch_assoc()) {
+        $retval[] = $row['CLASS_NUMBER'];
+    }
+    return $retval;
+}
+
+function get_class_details($conn, $class_number) {
+    $stmt = $conn->prepare(
+        "SELECT T.REAL_NAME AS TEACHER_NAME, E.TITLE AS TITLE, E.DESCRIPTION AS DESCRIPTION " .
+        "FROM TEACHER_USER T " .
+        "INNER JOIN EDU_CLASS E ON T.USER_NUMBER = E.TEACHER_NUMBER " .
+        "WHERE E.CLASS_NUMBER = ?;"
+    );
+    $stmt->bind_param("i", $class_number);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $assoc = $result->fetch_assoc();
+
+    return array(
+        "teacher_name" => $assoc["TEACHER_NAME"],
+        "class_title" => $assoc["TITLE"],
+        "class_description" => $assoc["DESCRIPTION"]
+    );
+}
+
+function get_all_classmates($conn, $class_number) {
+    $stmt = $conn->prepare(
+        "SELECT S.REAL_NAME AS STUDENT_NAME " .
+        "FROM STUDENT_USER S " .
+        "INNER JOIN EDU_CLASS E ON E.CLASS_NUMBER = ? " .
+        "INNER JOIN STUDENT_IN_CLASS SC ON SC.CLASS_NUMBER = E.CLASS_NUMBER " .
+        "WHERE S.USER_NUMBER = SC.STUDENT_NUMBER;"
+    );
+    $stmt->bind_param("i", $class_number);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $retval = array();
+    while ($row = $result->fetch_assoc()) {
+        $retval[] = $row['STUDENT_NAME'];
+    }
+    return $retval;
+}
 ?>
 <!DOCTYPE html> 
 <html>
