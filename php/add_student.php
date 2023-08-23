eb964dacb964f4ac3d7f347d751d4e4d5fc24253
William Kent <wjk011@gmail.com>
2023-08-23 10:33:00 -0400

    WIP improvement of add_student.php

--- php/add_student.php
+++ php/add_student.php
@@ -16,25 +16,40 @@ function get_teacher_id($conn, $user_name) {
     $row = $result->fetch_assoc();
     return $row["USER_NUMBER"];
 }
+
+function is_in_class($conn, $student_id, $class_id) {
+    $stmt = $conn->prepare(
+        "SELECT COUNT(*) AS COUNT FROM STUDENT_IN_CLASS WHERE STUDENT_NUMBER = ? AND CLASS_NUMBER = ?;"
+    );
+    $stmt->bind_param("ii", $student_id, $class_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    return $row["COUNT"] > 0;
+}
+
 // Check if the user is a teacher
 if (isset($_SESSION["role"]) && $_SESSION["role"] === 'teacher') {
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        // Process the assignment upload
         $classNumber = $_POST['classNumber'];
         $student = $_POST["studentNumber"];
 
-            // Insert assignment into the database
-            $insertQuery = "INSERT INTO STUDENT_IN_CLASS ( STUDENT_NUMBER, CLASS_NUMBER) VALUES (?, ?)";
+        if (is_in_class($conn, $student, $classNumber)) {
+            echo "That student is already in that class.";
+        } else {
+            // Insert student into the database
+            $insertQuery = "INSERT INTO STUDENT_IN_CLASS (STUDENT_NUMBER, CLASS_NUMBER, GRADE) VALUES (?, ?, 100)";
             $stmt = $conn->prepare($insertQuery);
             $stmt->bind_param("ii",  $student, $classNumber);
 
             if ($stmt->execute()) {
-                echo "Student uploaded successfully!";
+                echo "Student added successfully!";
             } else {
                 echo "Error adding student: " . $stmt->error;
             }
 
             $stmt->close();
+        }
     }
 } else {
     header("HTTP/1.1 403 Forbidden");
