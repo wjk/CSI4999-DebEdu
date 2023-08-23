108f878aabb4a3c3069d2b520951731c060d3ff5
William Kent <wjk011@gmail.com>
2023-08-22 21:37:09 -0400

    Update default character set in SQL script

--- database/create-database.sql
+++ database/create-database.sql
@@ -8,7 +8,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema DebEdu
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `DebEdu` DEFAULT CHARACTER SET utf8;
+CREATE SCHEMA IF NOT EXISTS `DebEdu` DEFAULT CHARACTER SET utf8mb4;
 USE `DebEdu` ;
 
 -- -----------------------------------------------------
